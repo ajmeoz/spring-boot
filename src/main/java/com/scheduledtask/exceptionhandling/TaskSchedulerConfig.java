b9ef70a57e5add4114a2220959f492eac63221ec
Martin Antl <jda2@atlas.cz>
2019-12-19 13:25:21 +0100

    Renamed configuration class and error handler class

--- src/main/java/com/scheduledtask/exceptionhandling/TaskSchedulerConfig.java
+++ src/main/java/com/scheduledtask/exceptionhandling/TaskSchedulerConfig.java
@@ -9,22 +9,22 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.util.ErrorHandler;
 
 @Configuration
-public class ApplicationConfiguration implements TaskSchedulerCustomizer {
+public class TaskSchedulerConfig implements TaskSchedulerCustomizer {
 
 	@Override
 	public void customize(ThreadPoolTaskScheduler taskScheduler) {
-		taskScheduler.setErrorHandler(new CustomErrorHandler());
+		taskScheduler.setErrorHandler(new TaskSchedulerExceptionHandler());
 	}
 
-	private static class CustomErrorHandler implements ErrorHandler {
+	private static class TaskSchedulerExceptionHandler implements ErrorHandler {
 
-		private static final Logger logger = LoggerFactory.getLogger(CustomErrorHandler.class);
+		private static final Logger logger = LoggerFactory.getLogger(TaskSchedulerExceptionHandler.class);
 
 		@Override
 		public void handleError(Throwable t) {
 			logger.error(ExceptionUtils.getMessage(t) + ", " + ExceptionUtils.getRootCauseMessage(t));
 		}
-		
+
 	}
-	
+
 }
