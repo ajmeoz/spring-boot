962d5cd186f1096a6b288e3ebb1b7cb58f0b433c
Martin Antl <jda2@atlas.cz>
2019-12-19 15:01:32 +0100

    Added comments

--- src/main/java/com/scheduledtask/exceptionhandling/TaskSchedulerConfig.java
+++ src/main/java/com/scheduledtask/exceptionhandling/TaskSchedulerConfig.java
@@ -8,9 +8,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.util.ErrorHandler;
 
+/**
+ * @author antlma
+ *
+ * Spring application configuration
+ * Inspired by https://stackoverflow.com/questions/41041536/universal-exception-handler-for-scheduled-tasks-in-spring-boot-with-java-conf
+ *
+ */
 @Configuration
 public class TaskSchedulerConfig implements TaskSchedulerCustomizer {
 
+	/**
+	 * Exception handling customization of a TaskScheduler instance
+	 */
 	@Override
 	public void customize(ThreadPoolTaskScheduler taskScheduler) {
 		taskScheduler.setErrorHandler(new TaskSchedulerExceptionHandler());
@@ -20,6 +30,9 @@ public class TaskSchedulerConfig implements TaskSchedulerCustomizer {
 
 		private static final Logger logger = LoggerFactory.getLogger(TaskSchedulerExceptionHandler.class);
 
+		/**
+		 * Handles the error occurred during execution of a task
+		 */
 		@Override
 		public void handleError(Throwable t) {
 			logger.error(ExceptionUtils.getMessage(t) + ", " + ExceptionUtils.getRootCauseMessage(t));
